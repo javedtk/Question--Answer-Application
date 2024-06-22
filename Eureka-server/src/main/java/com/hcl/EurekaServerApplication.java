fad29b959b4f11a0cdecdf033118a84a73d1bfbd
Javed Khan <61507772+javedtk@users.noreply.github.com>
2024-06-22 20:27:14 +0530

    Add files via upload

---
+++ Eureka-server/src/main/java/com/hcl/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.hcl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+
+//registry with which other applications can communicate  with annotations @EnableEurekaServer
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
